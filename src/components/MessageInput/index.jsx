9c32fee3b2e54d5ddb5c1f1c4c723d9c34db24bb
Guillaume Wuip <guillaume@wuips.com>
2017-10-25 23:55:21 +0200

    Listen on Enter key press

--- src/components/MessageInput/index.jsx
+++ src/components/MessageInput/index.jsx
@@ -4,12 +4,20 @@ import './MessageInput.scss';
 class MessageInput extends React.Component {
   constructor() {
     super();
-    this.onClick = this.onClick.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
-  onClick() {
+  handleClick() {
     this.props.clickHandler(this.input.value);
     this.input.value = null;
+  }
+
+  handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      this.props.clickHandler(this.input.value);
+      this.input.value = null;
+    }
   }
 
   render() {
@@ -19,12 +27,13 @@ class MessageInput extends React.Component {
           type="text"
           className="message-input-text"
           ref={(input) => { this.input = input; }}
+          onKeyPress={this.handleKeyPress}
         />
         <div className="message-input-send">
           <button
             type="button"
             className="button"
-            onClick={this.onClick}
+            onClick={this.handleClick}
           >
             →
           </button>
