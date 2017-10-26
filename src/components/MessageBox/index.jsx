00d871765a8fc72da0f9ba72653bde15685b5158
Guillaume Wuip <guillaume@wuips.com>
2017-10-26 08:32:29 +0200

    Better styles for message list

--- src/components/MessageBox/index.jsx
+++ src/components/MessageBox/index.jsx
@@ -6,15 +6,19 @@ import './MessageBox.scss';
 
 const MessageBox = ({ from, messages, send }) => (
   <div className="message-box">
-    <div>
-      {messages.map((message) => (
-        <Message
-          key={message.id}
-          text={message.text}
-          color={message.from === from}
-          right={message.from === from}
-        />
-      ))}
+    <h1 className="message-box-title">{from}</h1>
+    <div className="messages-list">
+      {messages
+        .reverse() // for css hack to keep scroll to bottom
+        .map((message) => (
+          <Message
+            key={message.id}
+            text={message.text}
+            color={message.from === from}
+            right={message.from === from}
+          />
+        ))
+      }
     </div>
     <MessageInput clickHandler={send} />
   </div>
